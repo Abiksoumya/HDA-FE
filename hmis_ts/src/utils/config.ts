b9bb9b0088b932dac610cece1449f9276b8f43be
Abiksoumya <soumyamohanty540@gmail.com>
2026-04-24 23:28:26 +0530

    prxy added

--- hmis_ts/src/utils/config.ts
+++ hmis_ts/src/utils/config.ts
@@ -1,4 +1,6 @@
 // export const API_BASE_URL = 'http://localhost:8000';
 // export const API_FALLBACK_URL = 'http://localhost:8000';
-export const API_BASE_URL = 'http://103.171.45.210:8000';
-export const API_FALLBACK_URL = 'http://103.171.45.210:8000';
+// export const API_BASE_URL = 'http://103.171.45.210:8000';
+// export const API_FALLBACK_URL = 'http://103.171.45.210:8000';
+export const API_BASE_URL = '/api';
+export const API_FALLBACK_URL = '/api';
